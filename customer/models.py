34f7a83002d14ff578c8583ce0aae4f38d37fda6
Victor Pereira <victorruan135@gmail.com>
2021-09-15 14:05:28 -0300

    Add classes on administration panel and correcting some errors

--- customer/models.py
+++ customer/models.py
@@ -12,7 +12,7 @@ class MenuItem(models.Model):
     def __str__(self):
         return self.name
     
-class Catergory(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
