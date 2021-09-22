b8489e84b8d031b331c56cf38788fc433100ab4f
Victor Pereira <victorruan135@gmail.com>
2021-09-22 11:35:40 -0300

    Adding routes to allauth

--- deliver/urls.py
+++ deliver/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from customer.views import Index, About, Order, OrderConfirmation, OrderPayConfirmation
